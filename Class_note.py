d200176c411289dcce54f92b4c3036d1fc6beb1d
miumiujiang12138 <129245663+miumiujiang12138@users.noreply.github.com>
2024-09-12 11:17:57 +0800

    update class_note.py

--- Class_note.py
+++ Class_note.py
@@ -35,5 +35,37 @@ user1=User('jennie','king',location='Korean',age=27)
 user1.describe_user()
 user1.greet_user()
 
-        
+# 使用类
+
+class User():
+    def __init__(self,first_name,last_name,**user_info):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_info = user_info
+        self.login_attempts = 0
+    def describe_user(self):
+        profile = {
+           # 'first_name' : self.first_name,
+           # 'last_name': self.last_name
+        }
+        profile['first_name'] = self.first_name
+        profile['last_name'] = self.last_name
+        for key,value in self.user_info.items():
+            profile[key] = value
+        print(profile)
+    def greet_user(self):
+        print('\nHello, '+self.first_name.title()+' '+self.last_name.title()+'!')
+    def increment_login_attempts(self):
+        self.login_attempts+=1
+        print('The user`s login times: '+str(self.login_attempts))
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+        print('The rest login time: '+str(self.login_attempts))
+user2 = User('jay','zhou',location = 'china',age=36)
+user2.describe_user()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+user2.increment_login_attempts()
+user2.reset_login_attempts()
+
         
